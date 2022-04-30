52103ca51ebf2f1a23b64213bb19971af2fb5950
gilbe96 <gilbertoa.acosta@gmail.com>
2022-04-30 01:56:30 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// start AOS
+AOS.init({
+  delay: 300,
+  duration: 1000,
+});
+
+let menu = document.getElementById("menu");
+let menu_bar = document.getElementById("menu-bar");
+menu_bar.addEventListener("click", function () {
+  menu.classList.toggle("menu-toggle");
+});
